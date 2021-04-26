874e0afa304ce76f5e794c8ea183da1c8df560eb
robbieflockhart <40343879@live.napier.ac.uk>
2021-04-26 19:15:03 +0100

    Arm and Disarm buttons and alarm status.

--- app/src/main/java/com/example/argondooralarm/MainActivity.java
+++ app/src/main/java/com/example/argondooralarm/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.argondooralarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -18,9 +22,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ParticleCloudSDK.init(this);
+        //ParticleCloudSDK.init(this);
         setContentView(R.layout.activity_main);
 
+        Button armBTN = (Button)findViewById(R.id.armBTN);
+        Button disarmBTN = (Button)findViewById(R.id.disarmBTN);
+        TextView statusTxt = (TextView)findViewById(R.id.statusTxt);
+        boolean alarmStatus = true;
+
+        if (alarmStatus == false)
+        {
+            armBTN.setVisibility(View.VISIBLE);
+            disarmBTN.setVisibility(View.INVISIBLE);
+            statusTxt.setText("DISARMED");
+            statusTxt.setTextColor(Color.parseColor("#F44336"));
+        }
+        else if (alarmStatus == true)
+        {
+            armBTN.setVisibility(View.INVISIBLE);
+            disarmBTN.setVisibility(View.VISIBLE);
+            statusTxt.setText("ARMED");
+            statusTxt.setTextColor(Color.parseColor("#4CAF50"));
+        }
+
+        armBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                armBTN.setVisibility(View.INVISIBLE);
+                disarmBTN.setVisibility(View.VISIBLE);
+                statusTxt.setText("ARMED");
+                statusTxt.setTextColor(Color.parseColor("#4CAF50"));
+
+            }
+        });
+        disarmBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                armBTN.setVisibility(View.VISIBLE);
+                disarmBTN.setVisibility(View.INVISIBLE);
+                statusTxt.setText("DISARMED");
+                statusTxt.setTextColor(Color.parseColor("#F44336"));
+            }
+        });
         /*Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Integer>() {
 
             public Integer callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
